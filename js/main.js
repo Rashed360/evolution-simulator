7f97a9e7c73f76ebb2ff2883fe05fb48ea5d5b8b
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-01-31 21:18:40 +0600

    update the manu bar transition problem

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 let openNav = document.getElementById("openNav");
 let main_dashboard = document.getElementById("main_dashboard");
-let sideNav = document.getElementById("side_menue");
+let side_menue = document.getElementById("side_menue");
 clickCount=1
 
 // For open and close side div 
 openNav.addEventListener("click", function () {
     if(clickCount == 1){
-        sideNav.style.width = "30%";
+        side_menue.style.width = "30%";
         main_dashboard.style.marginRight = "30%";
-        // sideNav.style.display = "block";
+        side_menue.style.visibility = "visible";
         clickCount=0
     }
     else{
         clickCount=1
-        sideNav.style.width = "0";
+        side_menue.style.width = "0";
         main_dashboard.style.marginRight = "0";
-        // sideNav.style.display = "none";
+        side_menue.style.visibility = "hidden";
     }
 });
 
